4afd001dcbded650891e8de4e7f9ff134125c6ad
hcavalle <harrison.cavallero@gmail.com>
2017-04-11 11:43:09 -0700

    many files

--- test.py
+++ test.py
@@ -1,7 +1,13 @@
-for i in range (1, 5000):
-    f = open(str(i) + '.out', 'w')
-    print "writing file " + str(i) + ".out"
-    f.write(str(i))
-    f.close()
+import os
+
+for i in range (1, 1000):
+    if not os.path.exists(str(i)):
+        os.makedirs(str(i))    
+    for n in range (1, 1100):
+        file = "{}/{}".format(str(i),str(n))
+        f = open(file + '.out', 'w+')
+        print "writing file " + str(i) + ".out"
+        f.write(str(i))
+        f.close()
 
 

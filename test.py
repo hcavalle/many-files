795fc37c762757346a5484bde42ae12b591d0ab7
hcavalle <harrison.cavallero@gmail.com>
2017-04-11 11:52:38 -0700

    add many dirs with files

--- test.py
+++ test.py
@@ -1,13 +1,17 @@
 import os
+from shutil import rmtree
 
-for i in range (1, 1000):
+for i in range (0, 10):
     if not os.path.exists(str(i)):
         os.makedirs(str(i))    
-    for n in range (1, 1100):
-        file = "{}/{}".format(str(i),str(n))
-        f = open(file + '.out', 'w+')
-        print "writing file " + str(i) + ".out"
-        f.write(str(i))
-        f.close()
+    else:
+        for n in range (0, 1100):
+            file = "{}/{}".format(str(i),str(n))
+            f = open(file + '.out', 'w+')
+            print "writing file " + str(i) + ".out"
+            f.write(str(i))
+            f.close()
+        # pass
+        # rmtree(str(i))
 
 
